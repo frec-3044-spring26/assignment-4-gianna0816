2c75c4ce1c6e5384fa375ecd815c4cc2c01fa4af
gianna0816 <gianna7alina@gmail.com>
2026-02-17 15:14:49 -0500

    Done for the day (3:08pm).

--- inClassAssignment2-17.R
+++ inClassAssignment2-17.R
@@ -16,4 +16,49 @@ ggplot(complete_old, aes(weight, hindfoot_length, color = plot_type)) +
 ggplot(complete_old, aes(plot_type, hindfoot_length,
                          fill = plot_type)) +
   geom_boxplot() +
-  scale_x_discrete(labels = label_wrap_gen(width = 10))
+  geom_jitter(aes(color = plot_type), alpha = 0.2) +
+  geom_boxplot() +
+  scale_x_discrete(labels = label_wrap_gen(width = 10)) +
+  theme_bw() +
+  theme(axis.title = element_text(size = 14, color = "red4"), legend.position = "none") +
+  labs(title = "Rodent Size by Plot Type", x = "Plot Type", y = "Hindfoot Length (mm)") +
+  facet_wrap(vars(sex), nrow = 1)
+
+species_count <- complete_old |>
+  group_by(species) |>
+  summarize(count = n()) |>
+  arrange(desc(count)) |>
+  slice(1:5)
+
+ggplot(complete_old, aes(plot_type, hindfoot_length,
+                         fill = plot_type)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = plot_type), alpha = 0.2) +
+  geom_boxplot() +
+  scale_x_discrete(labels = label_wrap_gen(width = 10)) +
+  theme_bw() +
+  theme(axis.title = element_text(size = 14, color = "red4"), legend.position = "none") +
+  labs(title = "Rodent Size by Plot Type", x = "Plot Type", y = "Hindfoot Length (mm)") +
+  facet_grid(rows = vars(sex), cols = vars(species))
+
+#Tidyingg data to 5 most common species
+species_count <- complete_old |>
+  group_by(species) |>
+  summarize(count = n()) |>
+  arrange(desc(count)) |>
+  slice(1:5)
+
+#Code for Five most common species
+complete_old |>
+  filter(species %in% species_count$species) |>
+  ggplot(aes(plot_type, hindfoot_length,
+                           fill = plot_type)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = plot_type), alpha = 0.2) +
+  geom_boxplot() +
+  scale_x_discrete(labels = label_wrap_gen(width = 10)) +
+  theme_bw() +
+  theme(axis.title = element_text(size = 14, color = "red4"), legend.position = "none") +
+  labs(title = "Rodent Size by Plot Type", x = "Plot Type", y = "Hindfoot Length (mm)") +
+  facet_grid(rows = vars(sex), cols = vars(species))
+
